b972b799239ce3a9bbb23883e4bef122713b3166
renananiceto <renan.a.ferreira94@gmail.com>
2021-06-04 18:53:16 -0300

    first comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class Header extends Component {
+  state = {
+    nome: 'Renan Aniceto',
+    idade: 26,
+    comidaFavorita: 'Lasanha',
+    musicas: ['Imagine Dragons  Nothing left to say','Titanic - My Heart Will Go On (Piano Cover)','The Game is Afoot'  ],
+    
+  }
+  render() {
+    return (
+      <div className='header'>
+        <h1>{this.state.nome}</h1>
+        <h2>{this.state.idade}</h2>
+        <h3>{this.state.comidaFavorita}</h3>
+        <ul className='list'>
+          <li>{this.state.musicas[0]}</li>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/mGv0ze0lHKA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <li>{this.state.musicas[1]}</li>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/KAQWzHokZXg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <li>{this.state.musicas[2]}</li>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/Q5GHpnQVkDc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </ul>
+      </div>
+    )
+  }
+};
 
-export default App;
+export default Header;
